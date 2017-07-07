ef4e08b94274d3404fe829ab7d2a65603dbf1bce
michael-mj-john <mbjohn@ucsc.edu>
2017-07-06 17:22:46 -0700

    labeled the turd

--- Rocky/playerUpdate.ino
+++ Rocky/playerUpdate.ino
@@ -1,12 +1,13 @@
 void gameUpdate( void ) {
 
   velocity = velocity + acceleration;
+ // This is a turd
   if( targetPixel > NUM_LEDS / 2 ) { 
     acceleration = acceleration - seconds^2;
   }
   else {
     acceleration = acceleration + seconds^2;
   }
-
+  
 }
 
